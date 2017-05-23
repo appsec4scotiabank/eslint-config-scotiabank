16fa45bbd2755404cf44dc59f094d199d3a1d07a
Rick Eyre <rick.eyre@outlook.com>
2017-05-23 12:42:54 -0400

    Add script tasks.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ module.exports = {
     'standard'
   ],
   rules: {
-    'semi': [2, 'always'],
-    'object-curly-spacing': [2, 'always'],
-    'array-bracket-spacing': [2, 'always'],
-    'space-before-function-paren': [2, 'never']
+    'semi': [ 2, 'always' ],
+    'object-curly-spacing': [ 2, 'always' ],
+    'array-bracket-spacing': [ 2, 'always' ],
+    'space-before-function-paren': [ 2, 'never' ]
   }
 };
